f1a76a95d3329f71496610ff5afa4744f2f352b3
Yutty Akiko <yutty.akiko@gmail.com>
2024-03-25 17:50:42 -0300

    Adicionar css na pasta static

--- webdev/urls.py
+++ webdev/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, re_path
+from django.urls import re_path
 from django.contrib import admin
 from mycontacts import views
 
 urlpatterns = [
-    re_path(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/', admin.site.urls),
     re_path(r'^$', views.show),
     re_path(r'^add/', views.add),
 ]
